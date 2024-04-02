e3b5d43c596182ea86f41cbb87b2eed1004866c1
Viktor Pakholok <pakholok.pn@ucu.edu.ua>
2024-04-02 14:27:58 +0300

    Added docstring and typehinting

--- Convert a linked list to a string/main.py
+++ Convert a linked list to a string/main.py
@@ -1,13 +1,13 @@
-''''''
+'''Solution to problem "Convert a linked list to a string"'''
 
 class Node():
-    ''''''
+    '''Basic class Node from Preloaded'''
     def __init__(self, data, next = None):
         self.data = data
         self.next = next
 
-def stringify(node):
-    ''''''
+def stringify(node: 'Node') -> str:
+    '''Accepts an argument node and returns a string representation of the node'''
     res = ''
     while node:
         res += str(node.data) + " -> "
