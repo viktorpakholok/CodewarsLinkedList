79c5951b45cfd680944afc10f208bc5ed2b8abd4
Viktor Pakholok <pakholok.pn@ucu.edu.ua>
2024-04-06 14:50:48 +0300

    Added docstrings and typehintings

--- Linked Lists - Get Nth Node/main.py
+++ Linked Lists - Get Nth Node/main.py
@@ -1,13 +1,13 @@
-''''''
+'''A solution of "Linked Lists - Get Nth Node"'''
 
 class Node(object):
     """Node class for reference"""
-    def __init__(self, data, next=None):
+    def __init__(self, data, next: 'Node'=None):
         self.data = data
         self.next = next
 
-def get_nth(node, index):
-    ''''''
+def get_nth(node, index: int) -> 'Node':
+    '''Returns the node at the specified index in a linked list'''
     if index < 0 or not node:
         raise ValueError
     count = 0
