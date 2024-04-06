6b0fca99256f9b6f7edbb7cc26e32fc29252d4cc
Viktor Pakholok <pakholok.pn@ucu.edu.ua>
2024-04-06 14:28:02 +0300

    Made a solution of "Linked Lists - Push & BuildOneTwoThree"

---
+++ Linked Lists - Push & BuildOneTwoThree/main.py
@@ -0,0 +1,16 @@
+''''''
+class Node:
+    ''''''
+    def __init__(self, data):
+        self.data = data
+        self.next = None
+
+def push(head, data):
+    ''''''
+    node = Node(data)
+    node.next = head
+    return node
+
+def build_one_two_three():
+    ''''''
+    return push(push(push(None, 3), 2), 1)
