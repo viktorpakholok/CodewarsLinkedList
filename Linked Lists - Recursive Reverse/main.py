80bb5247552c4111732280402e8787fc14cd9afc
Viktor Pakholok <pakholok.pn@ucu.edu.ua>
2024-04-08 10:00:41 +0300

    Added a solution of "Linked Lists - Recursive Reverse"

---
+++ Linked Lists - Recursive Reverse/main.py
@@ -0,0 +1,33 @@
+''''''
+
+class Node(object):
+    ''''''
+    def __init__(self, data=None):
+        self.data = data
+        self.next = None
+
+def reverse(head):
+    ''''''
+    if not head or not head.next:
+        return head
+    res = Node(head.data)
+    to_change = res
+
+    def reverse_def(head, count = 0):
+        nonlocal to_change
+        nonlocal res
+        if not head and count == 0:
+            return None
+
+        if head and head.next is not None:
+            reverse_def(head.next, count = count + 1)
+
+        to_change.data = head.data
+        if not count:
+            to_change.next = None
+        else:
+            to_change.next = Node(None)
+            to_change = to_change.next
+    reverse_def(head)
+
+    return res
