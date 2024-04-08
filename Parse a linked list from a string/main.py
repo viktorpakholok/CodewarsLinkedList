8859df21cc765bda77fe55b59d1226fb75df411c
Viktor Pakholok <pakholok.pn@ucu.edu.ua>
2024-04-08 21:36:19 +0300

    Added docstrings and typehinting for "Parse a linked list from a string"

--- Parse a linked list from a string/main.py
+++ Parse a linked list from a string/main.py
@@ -1,7 +1,7 @@
-''''''
+'''A solution of "Parse a linked list from a string"'''
 
 class Node:
-    ''''''
+    '''Represents a node in a linked list'''
     def __init__(self, data, next=None):
         self.data = data
         self.next = next
@@ -9,8 +9,8 @@ class Node:
     def __repr__(self) -> str:
         return f"Node({self.data}, {self.next})"
 
-def linked_list_from_string(s: str) -> list:
-    ''''''
+def linked_list_from_string(s: str) -> 'Node':
+    '''Constructs a linked list from a string representation'''
     list_s = s.split(' -> ')[::-1]
     res = None
     for el in list_s:
