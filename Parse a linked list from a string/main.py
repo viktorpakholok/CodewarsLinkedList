83c9b65e4ae111f00bf81ac1cce22d2b07fbc67e
Viktor Pakholok <pakholok.pn@ucu.edu.ua>
2024-04-08 22:45:44 +0300

    Deleted my own Node from "Parse a linked list from a string" to make my code passed the tests

--- Parse a linked list from a string/main.py
+++ Parse a linked list from a string/main.py
@@ -1,19 +1,9 @@
 '''A solution of "Parse a linked list from a string"'''
 
-class Node:
-    '''Represents a node in a linked list'''
-    def __init__(self, data, next=None):
-        self.data = data
-        self.next = next
-
-    def __repr__(self) -> str:
-        return f"Node({self.data}, {self.next})"
-
 def linked_list_from_string(s: str) -> 'Node':
     '''Constructs a linked list from a string representation'''
-    list_s = s.split(' -> ')[::-1]
+    list_s = s.split(' -> ')[::-1][1:]
     res = None
     for el in list_s:
-        if el != "None":
-            res = Node(int(el), res if res else None)
+        res = Node(int(el), res)
     return res
