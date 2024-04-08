47decd79429d8accd7115696b64c213f27756491
Viktor Pakholok <pakholok.pn@ucu.edu.ua>
2024-04-08 10:11:52 +0300

    Deleted prints from "Swap Node Pairs In Linked List"

--- Swap Node Pairs In Linked List/main.py
+++ Swap Node Pairs In Linked List/main.py
@@ -13,20 +13,13 @@ def swap_pairs(head):
     res = None
     while head and head.next:
         next_ = head.next.next
-        print(f'next: {next_}')
         first = head
-        print(f'first: {first}')
         second = head.next
         if not res:
             res = second
-        print(f'second: {second}')
         second.next = first
         first.next = next_
-        print(f'first.next: {first.next}')
-        print(f'jead: {head}')
         head = head.next
-        print(f'head: {head}')
         if head and head.next:
-            print('if')
             first.next = head.next
     return res
